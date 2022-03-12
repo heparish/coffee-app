3017a88e6bc596405c65d9ac81cce9522e4bd48f
Haley Parish <heparish@ucdavis.edu>
2022-03-11 16:00:50 -0800

    navigates on click now

--- src/components/AddCoffee.js
+++ src/components/AddCoffee.js
@@ -2,13 +2,15 @@ import React, {useContext} from 'react';
 import CoffeeForm from './CoffeeForm';
 import CoffeeContext from '../context/CoffeeContext';
 import { Navigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const AddCoffee = ({  }) => {
+  let navigate=useNavigate();
   const { coffees, setCoffee } = useContext(CoffeeContext);
   const handleOnSubmit = (coffee) => {
     setCoffee([coffee, ...coffees]);
     // navigate.push('/');
-    <Navigate to="/" />
+    navigate("/");
   };
 
   return (
